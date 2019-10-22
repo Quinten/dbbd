af99503420b9a153778172d16d476ce14b07ac14
Quinten Clause <quinten.clause@studioemma.eu>
2019-10-22 15:03:43 +0200

    Time toggle

--- index.js
+++ index.js
@@ -62,14 +62,22 @@ function createClip({
 var square = createClip({x: w/2, y: h/2});
 square.draw = function (time) {
     this.r = Math.sin(time / 600) * 2;
-    t.fillRect(-8, -8, 16, 16);
+    if (Math.floor(time / 2400) % 2) {
+        t.strokeRect(-8, -8, 16, 16);
+    } else {
+        t.fillRect(-8, -8, 16, 16);
+    }
 };
 p.push(square);
 
 // growing square
 square = createClip({x: w/2, y: h/4});
 square.draw = function (time) {
-    t.fillRect(Math.abs(Math.sin(time / 600)) * -48, -8, Math.abs(Math.sin(time / 600)) * 96, 16);
+    if (Math.floor(time / 2400) % 4) {
+        t.strokeRect(Math.abs(Math.sin(time / 600)) * -48, -8, Math.abs(Math.sin(time / 600)) * 96, 16);
+    } else {
+        t.fillRect(Math.abs(Math.sin(time / 600)) * -48, -8, Math.abs(Math.sin(time / 600)) * 96, 16);
+    }
 };
 p.push(square);
 
