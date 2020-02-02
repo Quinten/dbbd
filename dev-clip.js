b17d3e8e4cab97ef176de98afc59e0161545ca38
Quinten Clause <quintenclause@hotmail.com>
2020-02-02 12:14:41 +0100

    add some weird thing

--- dev-clip.js
+++ dev-clip.js
@@ -1,8 +1,8 @@
 // rotating square
 var square = createClip({x: w/2, y: h/2});
 square.draw = function (time) {
-    this.r = Math.sin(time / 600) * 2;
-    t.fillRect(-8, -8, 16, 16);
+    this.r = Math.sin(-time / 600) * Math.PI;
+    t.fillRect(64, -8, 16, 16);
     //t.strokeRect(-8, -8, 32, 32);
 };
 p.push(square);
--- first-clips.js
+++ first-clips.js
@@ -67,3 +67,11 @@ line.draw = function (time) {
     t.stroke();
 };
 p.push(line);
+
+square = createClip({x: w/2, y: h/2});
+square.draw = function (time) {
+    this.r = Math.sin(-time / 600) * Math.PI;
+    t.fillRect(64, -8, 16, 16);
+    //t.strokeRect(-8, -8, 32, 32);
+};
+p.push(square);
